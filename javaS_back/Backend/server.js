41d607e0e4c1ffb2356a44f8d819df8f74c23187
kanishk singhal <kanishk.singhal1801@gmail.com>
2024-04-11 12:43:39 +0530

    work

--- javaS_back/Backend/server.js
+++ javaS_back/Backend/server.js
@@ -2,25 +2,45 @@
 import express from "express";
 import cors from "cors";
 
-app.use(cors())
+const app = express();
 
-const app = express()
+app.use(cors());
 const port = 3000;
 
-app.get('/', (req , res)=>{
-    res.send("root");
-})
-app.get('/jokes', (req , res)=>{
-    const jokes = [
-        { id: 1, content: "Why did the developer go broke? Because he used up all his cache!" },
-        { id: 2, content: "Why was the JavaScript developer sad? Because he didn't know how to 'null' his feelings." },
-        { id: 3, content: "Why was the function so bent out of shape? It got called with too many arguments!" },
-        { id: 4, content: "Why did the programmer quit his job? Because he didn't get arrays!" },
-        { id: 5, content: "Why did the developer go to therapy? Because he had too many 'issues'!" },
-      ];
-    res.send(jokes)
-})
+app.get("/", (req, res) => {
+  res.send("root");
+});
+app.get("/api/jokes", (req, res) => {
+  const jokes = [
+    {
+      id: 1,
+      content:
+        "Why did the developer go broke? Because he used up all his cache!",
+    },
+    {
+      id: 2,
+      content:
+        "Why was the JavaScript developer sad? Because he didn't know how to 'null' his feelings.",
+    },
+    {
+      id: 3,
+      content:
+        "Why was the function so bent out of shape? It got called with too many arguments!",
+    },
+    {
+      id: 4,
+      content:
+        "Why did the programmer quit his job? Because he didn't get arrays!",
+    },
+    {
+      id: 5,
+      content:
+        "Why did the developer go to therapy? Because he had too many 'issues'!",
+    },
+  ];
+  res.send(jokes);
+});
 
-app.listen(port , ()=>{
-    console.log(`this are on ${port}`)
-})
+app.listen(port, () => {
+  console.log(`this are on ${port}`);
+});
--- javaS_back/Frontend/src/App.jsx
+++ javaS_back/Frontend/src/App.jsx
@@ -1,32 +1,31 @@
-import { useEffect, useState } from "react"
+import { useEffect, useState } from "react";
 import axios from "axios";
 
 function App() {
-  const [jokes , setJokes] = useState([]) 
+  const [joke, setJoke] = useState([]);
 
-  useEffect(()=>{
-    axios.get('https://zany-space-zebra-wx6gwpr4jvjhg4j-3000.app.github.dev/jokes')
-    .then((response)=>{
-      setJokes(response.data)
-    })
-    .catch((error)=>{
-      console.log(error)
-    })
-  })
+  useEffect(() => {
+    axios
+      .get("/api/jokes")
+      .then((response) => {
+        setJoke(response.data);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  }, []);
 
   return (
     <>
-      <h1 className="text-3xl font-bold">
-      total jokes are {jokes.length}
-    </h1>
-{
-    jokes.map((joke)=>{
-      <div key={joke.id}>
-        <h1>{joke.content}</h1>
-      </div>
-    })}
+      <h1 className="text-3xl font-bold">total jokes are {joke.length}</h1>
+
+      {joke.map((jokes) => (
+        <div key={jokes.id}>
+          <h1>{jokes.content}</h1>
+        </div>
+      ))}
     </>
-  )
+  );
 }
 
-export default App
+export default App;
