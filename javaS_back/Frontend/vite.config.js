c01a0929f0c5a7541851f87dbae92cfa454e5b90
kanishk singhal <kanishk.singhal1801@gmail.com>
2024-04-11 12:49:00 +0530

    work

--- javaS_back/Frontend/vite.config.js
+++ javaS_back/Frontend/vite.config.js
@@ -1,12 +1,12 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  server:{
-    proxy:{
-      '/api' : 'http://localhost:3000/',
-    }
-  }
+  server: {
+    proxy: {
+      "/api": "http://localhost:3000/",
+    },
+  },
   plugins: [react()],
-})
+});
