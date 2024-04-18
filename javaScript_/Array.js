f4c771dafde8bf81edefe8b14232bca532d5e02a
kanishk singhal <kanishk.singhal1801@gmail.com>
2024-04-18 10:06:12 +0530

    work

---
+++ javaScript_/Array.js
@@ -0,0 +1,28 @@
+//array
+// JavaScript arrays are resizable and can contain a mix of different data types.
+// JavaScript array-copy operations create shallow copies(share the same references).
+
+const arr1 = [1, 2, 3];
+let arr2 = new Array(1, 2, 3);
+// console.log(arr2[0]);
+// console.log(arr2);
+// arr2.push(4);
+// console.log(arr2);
+// let removedElement = arr2.pop();
+// console.log(arr2);
+// console.log(removedElement);
+// array methods
+// arr2.unshift(2);
+// console.log(arr2);
+// arr2.shift();
+// console.log(arr2);
+
+// console.log(arr2.includes(9));
+// console.log(arr2.includes(2));
+// console.log(arr2.indexOf(2));
+
+// const newArray = arr2.join();
+// console.log(arr2);
+// console.log(newArray);
+
+// slice and splice
